f06675d3d4185a7695fa9b57ee4dde52a3226898
Navya Shah <123navyashah@gmail.com>
2026-03-06 19:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -275,6 +275,8 @@ def health():
 # Initialize DB
 setup_database()
 
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["DEBUG"] = True
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
